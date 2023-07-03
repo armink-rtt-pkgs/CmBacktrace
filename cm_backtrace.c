6bd0982b2594a4ed3401ab7eb404e31d5b725cc8
yanghaiyong <yanghaiyong@rt-thread.com>
2023-07-03 13:55:43 +0800

    【修复】修复结构体成员变更后的编译报错问题

--- cm_backtrace.c
+++ cm_backtrace.c
@@ -215,7 +215,7 @@ static void get_cur_thread_stack_info(uint32_t sp, uint32_t *start_addr, size_t 
  */
 static const char *get_cur_thread_name(void) {
 #if (CMB_OS_PLATFORM_TYPE == CMB_OS_PLATFORM_RTT)
-    return rt_thread_self()->name;
+    return rt_thread_self()->parent.name;
 #elif (CMB_OS_PLATFORM_TYPE == CMB_OS_PLATFORM_UCOSII)
     extern OS_TCB *OSTCBCur;
 
